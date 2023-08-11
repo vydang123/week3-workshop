2bdf089c2f247423f6ba32151a2622378bfbd794
vydang123 <140564196+vydang123@users.noreply.github.com>
2023-08-11 13:49:27 +1000

    new work

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
-var express = require('express'); //used for routing
+var express = require('express');
 var app = express();
-var http = require('http').Server(app); //used to provide http functionality
-app.use(express.static(__dirname + '/www'));
+var bodyParser = require('body-parser');
 
-let server = http.listen(3000, function () {
-    let host = server.address().address;
-    let port = server.address().port;
-    console.log("My First Nodejs Server!");
-    console.log("Server listening on: "+ host + " port: " + port);
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/www'));
+app.listen(3000,()=>{
+    var d = new Date();
+    var n = d.getHours();
+    var m = d.getMinutes();
+    console.log("Server has been started at: "+ n + " : " + m);
+});
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/www/form.html');
     });
 
-app.get('/test', function (req, res) {
-    res.sendFile(__dirname + '/www/test.html');
+app.post('/api/login',function(req,res){
+    let users =[{'email':'abc@gmail.com','pwd':'123'}]
+    if (!req.body){
+        return res.sendStatus(400)
+    }
+    var customer = {};
+    customer.email = req.body.email;
+    customer.upwd = req.body.upwd;
+    customer.valid = false;
+    for(let i=0;i<users.length;i++){
+        if(req.body.email == users[i].email && req.body.upwd == users[i].pwd){
+            customer.valid = true;
+            }
+       }
+       console.log({ valid: customer.valid });
+       res.json({ valid: customer.valid });
+
 });
-
-// require("./routes/check.js").routeFunc(app);
-
--- www/script.js
+++ www/script.js
@@ -1,42 +1,43 @@
-const { type } = require("express/lib/response");
-
-$(document).ready(function() {
-    $("#loginform").submit(function(event){
+$(document).ready(function () {
+    console.log("ready");
+    $("#loginform").submit(function(event) {
         event.preventDefault();
         ajaxPost();
     });
-})
 
-function ajaxPost(){
-    var formdata= {
-        email: $("email").val(),
-        upwd: $("upwd").val()
-    }
-    $.ajax({
-        type: "POST",
-        contentType: "application/json",
-        url: window.location + "api/login",
-        data: JSON.stringify(FormData),
-        dataType: 'json',
-        success : function(customer) {
-            if(customer.valid == true){
-                $("#loginform").removeClass("fail");
-                $("#loginform").addClass("success");
-            }else{
-                $("#loginform").removeClass("success");
-                $("#loginform").addClass("fail");
+    function ajaxPost() {
+        var formData = {
+            email: $("#email").val(),
+            upwd: $("#upwd").val()
+        };
+        $.ajax({
+            type: "POST",
+            contentType: "application/json",
+            url: window.location + "api/login",
+            data: JSON.stringify(formData),
+            dataType: 'json',
+            success: function (customer) {
+                if (customer.valid == true) {
+                    window.location.href = '/test.html';
+                } else {
+                    $("#loginform").removeClass("success");
+                    $("#loginform").addClass("fail");
+                }
+                $("#postResultDiv").html("<p>" + "Post Successfully! <br>" + "Email Address: " + customer.email + "</br>" +
+                    "Password" + customer.upwd + "</br>" + "Valid User:" + customer.valid + "</p>");
+
+                // Reset FormData after Posting
+                resetData();
+            },
+            error: function (e) {
+                alert("Error!");
+                console.log("ERROR:", e);
             }
-            $("postResultDiv").html("<p>" + "Post Successfully! <br>" + "Email Address: " + customer.email + "<br>" + "Password: " +customer.upwd + "<br>" + "Valid User: " + customer.valid + "</p>");
-        },
-        error : function(e){
-            alert("Error!")
-            console.log("Error: ", e);
-        }
-    });
-    resetData();
-}
+        });
+    }
 
-function resetData(){
-    $("#email").val("");
-    $("#upwd").val("");
-}
+    function resetData() {
+        $("#email").val("");
+        $("#upwd").val("");
+    }
+});
